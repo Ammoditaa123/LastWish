856261951766f5c64a8d4da0add357679d8c7c58
ammmeowwww <himyemailid2025@gmail.com>
2026-06-26 13:38:24 +0530

    chore: install dotenv and update hardhat config to support protected L2 network deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,22 @@
 import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+dotenv.config();
+
+const L2_RPC_URL = process.env.L2_RPC_URL || "";
+const PRIVATE_KEY = process.env.PRIVATE_KEY && process.env.PRIVATE_KEY !== "PASTE_YOUR_TESTNET_PRIVATE_KEY_HERE"
+  ? [process.env.PRIVATE_KEY]
+  : [];
+
+const networks = {
+  hardhat: {},
+};
+
+if (L2_RPC_URL && PRIVATE_KEY.length > 0) {
+  networks.baseSepolia = {
+    url: L2_RPC_URL,
+    accounts: PRIVATE_KEY,
+  };
+}
 
 /** @type import('hardhat/config').HardhatUserConfig */
 export default {
@@ -11,7 +29,5 @@ export default {
       }
     }
   },
-  networks: {
-    hardhat: {},
-  }
+  networks: networks
 };
